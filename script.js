e78d9da4a1d7e5aa51b4379b7eea46d9574b3251
NMurari7 <murarichowdhary77@gmail.com>
2024-12-01 09:35:50 +0100

    Delete Feature

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const itemInput = document.querySelector('#item-input')
 const itemList = document.querySelector('#item-list')
 const itemForm = document.querySelector('#item-form')
+const clearBtn = document.querySelector("#clear")
+const filterItems = document.querySelector('.filter')
+
+const items = itemList.querySelectorAll('li')
 
 
 
@@ -21,6 +25,9 @@ function addItem(e) {
 
         console.log(li)
         itemList.appendChild(li)
+        checkUI()
+        itemInput.value = ''
+        
     }
 
     function createButton(classes) {
@@ -38,5 +45,64 @@ function addItem(e) {
 }
 
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item') && (confirm('Are you sure')))
+    {
+            e.target.parentElement.parentElement.remove()
+        
+    }
+    checkUI()
+}
+
+function clearAll(e) {
+    
+    // if(e.target.id)
+    // itemList.innerHTML = ''
+
+    // OR
+    if (confirm('Are you sure deleting everything')) {
+        while (itemList.firstChild ) {
+            itemList.removeChild(itemList.firstChild)
+        }
+    }
+    checkUI();
+}
+
+function filterFunc(e) {
+    const items = itemList.querySelectorAll('li')
+    const text = e.target.value.toLowerCase();
+    items.forEach(item => {
+        if (item.textContent.toLowerCase().includes(text)) {
+            item.style.display = 'flex'
+        }
+        else {
+            item.style.display = 'none'
+        }
+    } )
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll("li");
+    if (items.length === 0) {
+        clearBtn.style.display = 'none'
+        filterItems.style.display = "none";
+    }
+    else {
+        clearBtn.style.display = 'block'
+        // clearBtn.style.display = "block";
+        filterItems.style.display = "block";
+    }
+}
+
+
+
 //event listeners
 itemForm.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener("click", clearAll);
+filterItems.addEventListener('input', filterFunc)
+
+checkUI();
+
+// localStorage.setItem("name", "murari");
+// console.log(localStorage.getItem("name"));
